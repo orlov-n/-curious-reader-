1ddb27f8eedc8b53ffee61e8d011fc31331a90ec
Nick Orlov <ialwaysreply@gmail.com>
2022-08-17 06:52:18 -0700

    Update loading message

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,11 +5,9 @@ import Sidebar from "../Sidebar/Sidebar";
 import { IList, IBookLists, IError } from "../Interfaces";
 import TopBooks from "../TopBooks/TopBooks";
 import BookGenrePage from "../BookGenrePage/BookGenrePage";
-import { NavLink, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import BookIcon from "../assets/book.png";
 import loadingIcon from "../assets/orange-loading.gif";
-
 
 const App = () => {
   const [bookLists, setBookLists] = useState([]);
@@ -32,15 +30,13 @@ const App = () => {
         <div>
           <ErrorMessage />
         </div>
+      ) : !bookLists.length ? (
+        <div>
+          <img src={loadingIcon} className="loading-icon" />{" "}
+        </div>
       ) : (
-        !bookLists.length ? 
-       
-
-          ( <div><img src={loadingIcon} className="loading-icon" /> <h1>Page Loading</h1></div>):
-        
         <>
           <Sidebar bookLists={bookLists} />
-
           <Route
             exact
             path="/"
